7258facc767e5f51bb73c43259ebce892451ee52
Mohamed Zaghloul <69206680+Mohamed768830@users.noreply.github.com>
2020-10-16 21:51:29 +0200

    first commit

---
+++ android/app/src/main/kotlin/mz2139006j/news_app1/MainActivity.kt
@@ -0,0 +1,6 @@
+package mz2139006j.news_app1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,95 @@
+import 'dart:isolate';
+import 'package:flutter/material.dart';
+import 'package:news_app1/helper/get_categories.dart';
+import 'package:news_app1/model/category_model.dart';
+import 'package:news_app1/widgets/header.dart';
+
+import 'newsfeeds.dart';
+
+void main() => runApp(new HomePage());
+
+class HomePage extends StatelessWidget {
+  static String tag = 'home-page';
+
+
+
+  List<CategoryModel> categoriesList =  getCategories();
+
+
+
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: header(context,isApptitle: true,titletext:"NewsApp"),
+
+      body: new SafeArea(
+        child: Container(
+          color: Colors.black12,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Center(
+              child: new ListView.builder(
+                itemCount: categoriesList.length,
+                itemBuilder: (context, position) {
+                  return  Center(
+                    child: GestureDetector(
+                      onTap: (){
+                        var category = categoriesList[position].categoryName;
+                        Navigator.push(
+                            context,
+                            new MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                              new NewsFeedPage(category),
+                            ));
+                      },
+                      child: Container(
+
+                          child: Stack(
+                            children: <Widget>[
+                              Container(
+                                width: MediaQuery.of(context).size.width * 0.8,
+                                height: 200,
+                                decoration: BoxDecoration(
+                                  image:  DecorationImage(
+                                    image: AssetImage(categoriesList[position].imageUrl),
+                                    fit: BoxFit.cover,
+                                  ),
+                                  border: Border.all(
+                                    color: Colors.black,
+                                    width: 2,
+                                  ),
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+
+                              ),
+                              Container(
+                                alignment: Alignment.center,
+                                width: MediaQuery.of(context).size.width * 0.8,
+                                height: 200,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(6),
+                                  color: Colors.black26, // if we use decoration color should inside the decoration
+                                ),
+                                child: Text(
+                                  categoriesList[position].categoryName,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
